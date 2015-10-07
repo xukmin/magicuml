0a20e8bc06d8ae5395d6427c6052702f4f498690
Min Xu <xukmin@gmail.com>
2015-10-07 16:44:07 -0700

    Rename exported tarball.

--- export-source.sh
+++ export-source.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git archive \
     --format=zip \
-    --prefix=uml/ \
-    --output=../uml-source-MinXu-$(date +"%Y-%m-%d-%H-%M").zip HEAD .
+    --prefix=magicuml/ \
+    --output=../magicuml-source-$(date +"%Y-%m-%d-%H-%M").zip HEAD .
